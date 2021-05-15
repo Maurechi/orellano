afe17d78c30ea8b16bc2b21074523fa921f4bfe1
mauro guz <mauroguzbejar@gmail.com>
2021-05-15 17:54:03 +0200

    implemented user registration and front end validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import LoginScreen from './screens/LoginScreen';
 import CartScreen from './screens/CartScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import { Container } from 'react-bootstrap';
 
 const App = () => {
@@ -13,9 +15,11 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" exact component={HomeScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" exact component={HomeScreen} />
         </Container>
       </main>
       <Footer />
