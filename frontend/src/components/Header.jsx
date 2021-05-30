4bfbfc9012275e22229e5eee4becbeefbc995c23
mauro guz <mauroguzbejar@gmail.com>
2021-05-30 12:54:12 +0200

    pushed navdrop items to the front

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,98 +1,92 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 // import SearchBox from './SearchBox';
 // Algolia
 // import { SearchBox } from 'react-instantsearch-dom';
-import CustomSearchBox from '../components/CustomSearchBox';
-import algoliasearch from 'algoliasearch/lite';
+import CustomSearchBox from '../components/CustomSearchBox'
+import algoliasearch from 'algoliasearch/lite'
 
 //
-import { logout } from '../actions/userActions';
-import {
-  SEARCHING_ACTIVE,
-  SEARCHING_RESET,
-} from '../constants/searchConstants';
+import { logout } from '../actions/userActions'
+import { SEARCHING_ACTIVE, SEARCHING_RESET } from '../constants/searchConstants'
 
 const Header = () => {
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const logoutHandler = () => {
-    dispatch(logout());
-  };
+    dispatch(logout())
+  }
 
-  const searching = useSelector((state) => state.searching);
+  const searching = useSelector((state) => state.searching)
 
   return (
     <header>
       <Navbar
-        className="black-marble"
-        variant="dark"
-        expand="lg"
+        className='black-marble'
+        variant='dark'
+        expand='lg'
         collapseOnSelect
       >
         <Container>
           <LinkContainer
-            to="/"
+            to='/'
             onClick={() => dispatch({ type: SEARCHING_RESET })}
           >
-            <Navbar.Brand className="black">Macellaio</Navbar.Brand>
+            <Navbar.Brand className='black'>Macellaio</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
             {/* <SearchBox onChange={(e) => searchHandler(e.target.value)} /> */}
             <CustomSearchBox />
             <LinkContainer
-              to=""
+              to=''
               onClick={() => dispatch({ type: SEARCHING_ACTIVE })}
             >
-              <Nav.Link className="black scale ml-auto">
+              <Nav.Link className='black scale ml-auto'>
                 <strong>Browse the Collection</strong>
               </Nav.Link>
             </LinkContainer>
-            <Nav className="ml-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link className="black grow">
-                  <i className="fas fa-shopping-cart"></i>Cart
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link className='black grow'>
+                  <i className='fas fa-shopping-cart'></i>Cart
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
                 <NavDropdown
-                  className="black grow"
+                  className='black grow front'
                   title={userInfo.name}
-                  id="user"
+                  id='user'
                 >
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item className="black">
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item className='black'>
                       Profile
                     </NavDropdown.Item>
                   </LinkContainer>
-                  <NavDropdown.Item
-                    className="black grow"
-                    onClick={logoutHandler}
-                  >
+                  <NavDropdown.Item className='black' onClick={logoutHandler}>
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link className="black grow">
-                    <i className="fas fa-user"></i>Sign in
+                <LinkContainer to='/login'>
+                  <Nav.Link className='black grow'>
+                    <i className='fas fa-user'></i>Sign in
                   </Nav.Link>
                 </LinkContainer>
               )}
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="admin" id="adminmenu">
-                  <LinkContainer to="/admin/userlist">
+                <NavDropdown title='admin' id='adminmenu' className='front'>
+                  <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/admin/productlist">
+                  <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/admin/orderlist">
+                  <LinkContainer to='/admin/orderlist'>
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
@@ -102,7 +96,7 @@ const Header = () => {
         </Container>
       </Navbar>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
